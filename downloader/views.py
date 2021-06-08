04e1bdc7da521c40ef99bf34fc852436793ca796
Yagna Yaswanth <yaswanth.yagna@gmail.com>
2021-06-08 16:43:11 +0530

    chnaged

--- downloader/views.py
+++ downloader/views.py
@@ -15,10 +15,18 @@ def home(request):
 def ytb_down(request):
     if request.method == 'POST':
         url = request.POST.get('ylink')
-        youtube = pytube.YouTube(url)
+        youtube = pytube.YouTube(url,on_progress_callback=progress_Check)
         video = youtube.streams.get_highest_resolution()
         var = Embedder()
+        global file_size
+        file_size = video.filesize
+        context = {"link": var(url),"title":youtube.title,"completed":"Your  video has been downloaded"}  
         video.download()
-        context = {"link": var(url)}
         return render(request, 'pages/home.html', context)
     return render(request, 'pages/home.html')
+
+
+def progress_Check(stream = None, chunk = None, file_handle = None, remaining = None):
+    #Gets the percentage of the file that has been downloaded.
+    percent = (100*(file_size-remaining))/file_size
+    print("{:00.0f}%  downloaded".format(percent))
